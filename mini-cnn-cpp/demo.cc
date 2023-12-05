f5dfed5bebe57d5e2cb5bbf568478cea69a67ed9
trhieung <nthieu.ngtr@gmail.com>
2023-12-05 23:39:19 +0700

    s

--- mini-cnn-cpp/demo.cc
+++ mini-cnn-cpp/demo.cc
@@ -27,7 +27,7 @@
 
 int main() {
   // data
-  MNIST dataset("../data/mnist/");
+  MNIST dataset("CSC14120/mini-cnn-cpp/data/mnist/");
   dataset.read();
   int n_train = dataset.train_data.cols();
   int dim_in = dataset.train_data.rows();
