3889e3e6cfeadbf6713ce380cf497417cb1d6fbd
trhieung <nthieu.ngtr@gmail.com>
2023-12-12 22:03:19 +0700

    s

--- cuda_on_colab/demo.cu
+++ cuda_on_colab/demo.cu
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "src/optimizer/sgd.h"
+#include "src/kernel/HW0.h"
+#include "src/kernel/HW0.cu"
 
 __global__ void myKernel(void) {
 	printf("hello from kernel\n");
@@ -9,5 +11,6 @@ int main(void) {
 	myKernel <<<2, 2>>>();
 	cudaDeviceSynchronize()
 	printf("Hello CUDA!\n");
+	printDeviceInfo();
 	return 0;
 }
