a2c2594a0a5672dceae5d69f96a013e584212ad5
trhieung <nthieu.ngtr@gmail.com>
2023-12-14 10:45:29 +0700

    s

--- sad1/demo.cc
+++ sad1/demo.cc
@@ -4,6 +4,14 @@
 
 
 int main(void) {
+	MNIST dataset("CSC14120/sad1/data/fashion-mnist/");
+	int n_train = dataset.train_data_col;
+	int dim_in = dataset.train_data_dim.first * dataset.train_data_dim.second;
+	std::cout << "fashion-mnist train number: " << n_train << std::endl;
+  	std::cout << "fashion-mnist test number: " << dataset.test_data_col << std::endl;
+
+	std::cout << "train dim: " << dim_in << std::endl;
+  
 	std::cout << "sad1" << std::endl;
     return 0;
 }
