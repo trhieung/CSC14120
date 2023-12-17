b2a8f7698f8d74373d54fba8e7abc16db7aadb45
trhieung <nthieu.ngtr@gmail.com>
2023-12-17 14:29:45 +0700

    s

--- cuda_on_colab/demo.cc
+++ cuda_on_colab/demo.cc
@@ -1,11 +1,14 @@
 #include <iostream>
-#include "./src/kernel/cuda_file.cuh"  // Include the CUDA header file
+
+// Include the CUDA header file
+#include "./src/kernel/cuda_file.cuh"  
+#include "./src/kernel/HW0.cuh"
 
 int main() {
     std::cout << "Hello, world!" << std::endl;
 
     // Call the CUDA function
     runCudaFunction();
-
+    printDeviceInfo();
     return 0;
 }
